f5d2a3715a9971e368962b685c09d235fd1482b2
sabeehkc <wg457129@gmail.com>
2024-03-23 17:16:08 +0530

    start all code format

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const admin_route = express();
 const auth = require("../middleware/adminAuth");
 const adminController = require("../controllers/adminController");
@@ -6,78 +6,84 @@ const productController = require("../controllers/productController");
 const categoryController = require("../controllers/categoryController");
 
 //----------------- set view engine -----------------//
-admin_route.set('view engine','ejs');
-admin_route.set('views','./views/admin');
-
+admin_route.set("view engine", "ejs");
+admin_route.set("views", "./views/admin");
 
 //----------------- Multer -----------------//
-const multer =require('multer');
+const multer = require("multer");
 const path = require("path");
 
 const storage = multer.diskStorage({
-    destination:function(req,file,cb){
-        cb(null,path.join(__dirname, '../public/product_images'));
-    },
-    filename:function(req,file,cb){
-        const name = Date.now()+'-'+file.originalname;
-        cb(null,name);
-    }
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "../public/product_images"));
+  },
+  filename: function (req, file, cb) {
+    const name = Date.now() + "-" + file.originalname;
+    cb(null, name);
+  },
 });
 
-const upload = multer({storage:storage});
-
+const upload = multer({ storage: storage });
 
 //----------------- load admin login page -----------------//
-admin_route.get('/',auth.isLogout,adminController.loginload);
-admin_route.post('/loginpost',adminController.Loginverifying);
+admin_route.get("/", auth.isLogout, adminController.loginload);
+admin_route.post("/loginpost", adminController.Loginverifying);
 
-admin_route.use(auth.isLogin)
+admin_route.use(auth.isLogin);
 
 //----------------- Admin Dashboard -----------------//
-admin_route.get('/dashboard',adminController.loadDashboard); 
-
+admin_route.get("/dashboard", adminController.loadDashboard);
 
 //----------------- load customer page -----------------//
-admin_route.get('/customers',adminController.loadCustomer);
+admin_route.get("/customers", adminController.loadCustomer);
 //----------------- block and unblock customer(post) -----------------//
-admin_route.post('/block-user', adminController.blockUser);
-
+admin_route.post("/block-user", adminController.blockUser);
 
 //----------------- load category page -----------------//
-admin_route.get('/category', categoryController.loadCategory);
+admin_route.get("/category", categoryController.loadCategory);
 
 //----------------- load Addcategory page (get,post) -----------------//
-admin_route.get('/category/add',categoryController.loadAddCategory);
-admin_route.post('/category/add', categoryController.addCategory);
+admin_route.get("/category/add", categoryController.loadAddCategory);
+admin_route.post("/category/add", categoryController.addCategory);
 
 //----------------- load Editcategory page (get,post) -----------------//
-admin_route.get('/category/edit/:id', categoryController.LoadEditCategory);
-admin_route.post('/category/edit/:id', categoryController.editCategory);
+admin_route.get("/category/edit/:id", categoryController.LoadEditCategory);
+admin_route.post("/category/edit/:id", categoryController.editCategory);
 
 //-----------------  Categories  active and blocked -----------------//
-admin_route.post('/category/toggle/:id', categoryController.toggleCategoryStatus);
+admin_route.post(
+  "/category/toggle/:id",
+  categoryController.toggleCategoryStatus
+);
 
+//-----------------  load Product page  -----------------//
+admin_route.get("/products", productController.loadProducts);
 
-//-----------------  load Product page  -----------------//   
-admin_route.get('/products',productController.loadProducts);
+admin_route.get("/products/add", productController.loadAddProducts);
+admin_route.post(
+  "/products/addpost",
+  upload.array("images", 4),
+  productController.addProduct
+);
 
-admin_route.get('/products/add',productController.loadAddProducts);
-admin_route.post('/products/addpost',upload.array('images',4),productController.addProduct);
+admin_route.get("/products/edit/:id", productController.loadEditProduct);
+admin_route.post(
+  "/products/edit/:id",
+  upload.array("images", 4),
+  productController.editProduct
+);
 
-admin_route.get('/products/edit/:id',productController.loadEditProduct);
-admin_route.post('/products/edit/:id', upload.array('images', 4),productController.editProduct);
+admin_route.delete("/productsdelete", productController.deleteImage);
 
-admin_route.delete('/productsdelete', productController.deleteImage);
+admin_route.post("/products/toggle/:id", productController.toggleProductStatus);
 
-admin_route.post('/products/toggle/:id',productController.toggleProductStatus);
+admin_route.get("/orders", adminController.loadOrders);
+admin_route.post(
+  "/change-product-status/:orderId/:productId",
+  adminController.ChangeOrderStatus
+);
 
-admin_route.get('/orders',adminController.loadOrders);
-admin_route.post('/change-product-status/:orderId/:productId',adminController.ChangeOrderStatus)
-
-admin_route.get('/logout',adminController.logout);
-
-
-
+admin_route.get("/logout", adminController.logout);
 
 //----------------- export admin route -----------------//
 module.exports = admin_route;
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,93 +1,127 @@
-const express = require('express');
+const express = require("express");
 const user_route = express();
 
 const userController = require("../controllers/userController");
 
-const productController = require("../controllers/productController")
- 
+const productController = require("../controllers/productController");
+
 const profileController = require("../controllers/profileController");
 
 //----------------- Login With Google -----------------//
-const passport = require('passport');
-require('../passport');    //passport.js
+const passport = require("passport");
+require("../passport"); //passport.js
 
 user_route.use(passport.initialize());
 user_route.use(passport.session());
 
-user_route.get('/auth/google',passport.authenticate('google',{scope:
-    [ 'email', 'profile']
-}));
+user_route.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
 
-user_route.get('/auth/google/callback',
-    passport.authenticate('google', {
-        successRedirect: '/success',
-        failureRedirect: '/failure'
-}));
+user_route.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/success",
+    failureRedirect: "/failure",
+  })
+);
 
-user_route.get('/success', userController.successGoogleLogin);
+user_route.get("/success", userController.successGoogleLogin);
 
-user_route.get('/failure', userController.failureGoogleLogin);
+user_route.get("/failure", userController.failureGoogleLogin);
 
-//----------------- Set view engine -----------------// 
-user_route.set('view engine', 'ejs');
-user_route.set('views', './views/users');
+//----------------- Set view engine -----------------//
+user_route.set("view engine", "ejs");
+user_route.set("views", "./views/users");
 
-const auth = require('../middleware/userAuth');
+const auth = require("../middleware/userAuth");
 
 //----------------- Home page -----------------//
-user_route.get('/',auth.isBlock,userController.loadHome);
+user_route.get("/", auth.isBlock, userController.loadHome);
 
 //----------------- Register routes -----------------//
-user_route.get('/register',auth.logUser, userController.loadRegister);
-user_route.post('/register', auth.logUser,userController.insertUser);
-
+user_route.get("/register", auth.logUser, userController.loadRegister);
+user_route.post("/register", auth.logUser, userController.insertUser);
 
 //----------------- OTP -----------------//
-user_route.get('/otp-verification', userController.loadOtp);
-user_route.post('/verify-otp', userController.verifyOtp);
-user_route.get('/resend-otp',userController.resendOtp);
+user_route.get("/otp-verification", userController.loadOtp);
+user_route.post("/verify-otp", userController.verifyOtp);
+user_route.get("/resend-otp", userController.resendOtp);
 
 //----------------- Back Register page -----------------//
-user_route.get('/back-register',auth.logUser,userController.backRegister);
+user_route.get("/back-register", auth.logUser, userController.backRegister);
 
-//----------------- user Login routes -----------------// 
-user_route.get('/login',auth.logUser,userController.loadlogin);
-user_route.post('/login-verify',auth.logUser,userController.verifyLogin);
+//----------------- user Login routes -----------------//
+user_route.get("/login", auth.logUser, userController.loadlogin);
+user_route.post("/login-verify", auth.logUser, userController.verifyLogin);
 
-
-user_route.get('/all-products',auth.isBlock,productController.loadAllProduct); 
-user_route.get('/product/:id',auth.isBlock,productController.product);
+user_route.get("/all-products", auth.isBlock, productController.loadAllProduct);
+user_route.get("/product/:id", auth.isBlock, productController.product);
 
 //----------------- Logout -----------------//
-user_route.get('/logout',userController.logOut);
+user_route.get("/logout", userController.logOut);
 
 //----------------- About page -----------------//
-user_route.get('/about',auth.isBlock,userController.loadAbout);
+user_route.get("/about", auth.isBlock, userController.loadAbout);
 
-user_route.get('/profile/',auth.checkuser,auth.isBlock,profileController.loadProfile);
-user_route.post('/profile/:id',profileController.editProfile);
-user_route.post('/profile',profileController.changePassword);
+user_route.get(
+  "/profile/",
+  auth.checkuser,
+  auth.isBlock,
+  profileController.loadProfile
+);
+user_route.post("/profile/:id", profileController.editProfile);
+user_route.post("/profile", profileController.changePassword);
 
-user_route.get('/address',auth.checkuser,auth.isBlock,profileController.loadAddress);
-user_route.post('/address',profileController.addAddress);
+user_route.get(
+  "/address",
+  auth.checkuser,
+  auth.isBlock,
+  profileController.loadAddress
+);
+user_route.post("/address", profileController.addAddress);
 
-user_route.get('/edit-address/:id',auth.checkuser,auth.isBlock,profileController.loadEditAddress);
-user_route.post('/edit-address/:id',profileController.editAddress);
-user_route.get('/delete-address/:id',profileController.deleteAddress);
+user_route.get(
+  "/edit-address/:id",
+  auth.checkuser,
+  auth.isBlock,
+  profileController.loadEditAddress
+);
+user_route.post("/edit-address/:id", profileController.editAddress);
+user_route.get("/delete-address/:id", profileController.deleteAddress);
 
-user_route.get('/cart',auth.checkuser,auth.isBlock,userController.LoadCart);
-user_route.get('/cart-Products/:id',userController.addProductsCart);
-user_route.get('/cart/remove',userController.deleteCartProduct);
-user_route.put('/updateQuantity', userController.updateProductQuantity);
+user_route.get("/cart", auth.checkuser, auth.isBlock, userController.LoadCart);
+user_route.get("/cart-Products/:id", userController.addProductsCart);
+user_route.get("/cart/remove", userController.deleteCartProduct);
+user_route.put("/updateQuantity", userController.updateProductQuantity);
 
-user_route.get('/check-out',auth.checkuser,auth.isBlock,userController.LoadCheckOut);
+user_route.get(
+  "/check-out",
+  auth.checkuser,
+  auth.isBlock,
+  userController.LoadCheckOut
+);
 
-user_route.post('/order-placed',userController.placeOrder);
-user_route.get('/thank-you',auth.checkuser,auth.isBlock,userController.ThankYou);
-user_route.get('/orders',auth.checkuser,auth.isBlock,userController.loadOrders);
+user_route.post("/order-placed", userController.placeOrder);
+user_route.get(
+  "/thank-you",
+  auth.checkuser,
+  auth.isBlock,
+  userController.ThankYou
+);
+user_route.get(
+  "/orders",
+  auth.checkuser,
+  auth.isBlock,
+  userController.loadOrders
+);
 
-user_route.get('/filter-category/:id',auth.isBlock,productController.FilterCategory);
-
+user_route.get(
+  "/filter-category/:id",
+  auth.isBlock,
+  productController.FilterCategory
+);
 
 //----------------- export user route -----------------//
 module.exports = user_route;
